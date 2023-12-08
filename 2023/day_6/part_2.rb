91f4a4427fbdc0fb622501d7d8c691bbdb2e2efc
Saurabh Kamboj <skamboj97@gmail.com>
2023-12-08 19:07:20 +0530

    Add file

--- 2023/day_6/part_2.rb
+++ 2023/day_6/part_2.rb
@@ -5,11 +5,35 @@ input = File.read('input.txt')
 
 time, distance = input.split("\n").map { |row| row.split(':')[1].gsub(/\s/, '').to_i }
 
-def ways_product(time, record_distance)
+def ways(time, record_distance)
   (0..time).select do |current_time|
     speed, time_left = current_time, time - current_time
     speed * time_left > record_distance
   end.count
 end
 
-p ways_product(time, distance)
+# Quadratic equation
+=begin
+  - time_left = time - speed
+  - distance = time_left * speed
+    - distance = (time - speed) * speed
+    - speeed**2 - time*speed + distance = 0
+    - (ax^2 + bx + c=0)
+
+  - Quadratic formula
+  - x1 = (-b + sqrt(b**2 - 4ac))/2a
+  - x2 = (-b - sqrt(b**2 - 4ac))/2a
+    - a = 1
+    - x = speed
+    - b = -time
+    - c = distance + 1
+=end
+
+def ways(time, record_distance)
+  max_speed = (time + Math.sqrt(time**2 - 4*(record_distance)))/2
+  min_speed = (time - Math.sqrt(time**2 - 4*(record_distance)))/2
+
+  max_speed.ceil - min_speed.floor - 1
+end
+
+p ways(time, (distance + 1))
